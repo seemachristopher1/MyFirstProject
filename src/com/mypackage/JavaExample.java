bf05429a738305fcc696ce4d627127a6d225dfa0
user <user@DESKTOP-43BH1VD.home>
2021-02-18 06:49:23 -0800

    my first git checkin

---
+++ src/com/mypackage/JavaExample.java
@@ -0,0 +1,11 @@
+package com.mypackage;
+
+public class JavaExample {
+
+	public static void main(String[] args) {
+		
+		System.out.println("H There");
+
+	}
+
+}
---
+++ src/com/mypackage/SeemaFirstJavaExample.java
@@ -0,0 +1,15 @@
+package com.mypackage;
+
+public class SeemaFirstJavaExample {
+
+	public static void main(String[] args) {
+		
+		System.out.println(" Hi to Seema");
+
+	}
+	
+	public void mymethod() {
+		
+	}
+
+}
